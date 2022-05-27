c750f271aa1d6c9ccf04f48d6f93abaa871410ba
ChrisMak <cmakride@gmail.com>
2022-05-27 19:10:14 -0400

    Projectile Create On Click

--- index.js
+++ index.js
@@ -18,9 +18,35 @@ class Player {
   draw(){
     ctx.beginPath()
     ctx.arc(this.x,this.y,this.radius,0, Math.PI * 2,false)
+    ctx.fillStyle = this.color
     ctx.fill()
   }
 }
 
-const player = new Player(100,100, 30, 'blue')
+class Projectile{
+  constructor(x,y, radius,color,velocity){
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+  draw(){
+    ctx.beginPath()
+    ctx.arc(this.x,this.y,this.radius,0, Math.PI * 2,false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
+//!dynamically calculate what x and y should be based on the width and size of the canvas
+const x = canvas.width/2
+const y = canvas.height/2
+
+const player = new Player(x,y, 30, 'blue')
 player.draw()
+
+window.addEventListener("click",(event)=>{
+  const projectile = new Projectile(event.clientX,event.clientY,5,'red',null)
+  projectile.draw()
+})
